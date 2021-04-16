bd795f0ccfaf649944444b9c8dfe9e0e98e8fdfd
sosso21 <sofianetop21.st@gmail.com>
2021-04-16 22:08:27 +0100

    corrextio orthograph

---
+++ _static/_next/static/webpack/pages/index.9fcd3069e70e57ed1ec9.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./pages/api/fr.json":
+/*!***************************!*\
+  !*** ./pages/api/fr.json ***!
+  \***************************/
+/*! exports provided: urlServer, home, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"urlServer\":\"https://fakebook-admin.herokuapp.com\",\"home\":{\"title\":\"facebook\",\"description\":\"Avec Facebook, partagez et restez en contact avec votre entourage.\",\"input\":{\"email\":\"Adresse e-mail ou numéro de tél.\",\"pass\":\"Mot de passe\",\"buttonlogin\":\"Connexion\",\"mdpMissed\":\"Mot de passe oublié ?\",\"btnSignup\":\"Crée un compte\"}}}");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9wYWdlcy9pbmRleC45ZmNkMzA2OWU3MGU1N2VkMWVjOS5ob3QtdXBkYXRlLmpzIiwic291cmNlUm9vdCI6IiJ9
